d2117c92288522ceb182c3b40a43ec7f3e2938fd
mwhite98 <mylawhite@outlook.com>
2020-03-30 22:27:34 -0700

    base code

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # perceptron-art
 Project for COGS 300.
+
+### Running Locally
+- Make sure you have [Node.js](https://nodejs.org/en/) installed onto your computer.
+- Do new work on your own branch!
+- To make sure you have everything installed locally that this project uses, run:
+```
+npm i
+
+```
+- To run this project / develop locally:
+```
+browser-sync start --server -f -w
+Navigate to http://localhost:3000/
+```
+- Tap the screen to allow mic input, and then make noise to watch what happens!
+
+### Project TODOs
+- [x] Figure out tech stack / base code
+- [ ] Determine what the Perceptron should 'learn'
+    - What do the weights need to be moving to? What is the end goal?
+- [ ] Figure out how to better integrate perceptron algorithm with output visuals
+- [ ] Experiment on own branches to see what sorts of visuals we can all come up with!
--- drawing.js
+++ drawing.js
@@ -1,37 +1,8 @@
-// let mic;
+/*
+    Base code from "The Nature of Code", Daniel Shiffman, http://natureofcode.com
+    Code based on text "Artificial Intelligence", George Luger
 
-// function setup() {
-//   createCanvas(710, 200);
-
-//   // Create an Audio input
-//   mic = new p5.AudioIn();
-
-//   // start the Audio Input.
-//   // By default, it does not .connect() (to the computer speakers)
-//   mic.start();
-// }
-
-// function draw() {
-//   background(200);
-
-//   // Get the overall volume (between 0 and 1.0)
-//   let vol = mic.getLevel();
-//   fill(127);
-//   stroke(0);
-
-//   // Draw an ellipse with height based on volume
-//   let h = map(vol, 0, 1, height, 0);
-//   ellipse(width / 2, h - 25, 50, 50);
-// 
-
-// The Nature of Code
-// Daniel Shiffman
-// http://natureofcode.com
-
-// Simple Perceptron Example
-// See: http://en.wikipedia.org/wiki/Perceptron
-
-// Code based on text "Artificial Intelligence", George Luger
+*/
 
 // A list of points we will use to "train" the perceptron
 let training = new Array(2000);
@@ -85,34 +56,7 @@ function draw() {
   fill(255);
   text('tap to start', width/2, 20);
 
-  micLevel = mic.getLevel();
-  let y = height - micLevel * height;
-
-  // Draw the line
-  // strokeWeight(1);
-  // stroke(255);
-  // let x1 = map(xmin, xmin, xmax, 0, width);
-  // let y1 = map(f(xmin), ymin, ymax, height, 0);
-  // let x2 = map(xmax, xmin, xmax, 0, width);
-  // let y2 = map(f(xmax), ymin, ymax, height, 0);
-  // line(x1, y1, x2, y2);
-
-  // Draw the line based on the current weights
-  // Formula is weights[0]*x + weights[1]*y + weights[2] = 0
-  // stroke(255);
-  // strokeWeight(2);
-  // let weights = ptron.getWeights();
-  // x1 = xmin;
-  // y1 = (-weights[2] - weights[0] * x1) / weights[1];
-  // x2 = xmax;
-  // y2 = (-weights[2] - weights[0] * x2) / weights[1];
-
-  // x1 = map(x1, xmin, xmax, 0, width);
-  // y1 = map(y1, ymin, ymax, height, 0);
-  // x2 = map(x2, xmin, xmax, 0, width);
-  // y2 = map(y2, ymin, ymax, height, 0);
-  // line(x1, y1, x2, y2);
-
+  volume = mic.getLevel();
 
   // Train the Perceptron with one "training" point at a time
   ptron.train(training[count].input, training[count].output);
@@ -128,7 +72,8 @@ function draw() {
     if (guess > 0) noFill();
 
     let x = map(training[i].input[0], xmin, xmax, 0, width);
-    // let y = map(training[i].input[1], ymin, ymax, height, 0);
-    ellipse(x, y, 8, 8);
+    let y = map(training[i].input[1], ymin, ymax, height, 0);
+
+    ellipse(x, y, volume * 40, volume * 40);
   }
 }
