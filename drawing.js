10c1002d5b8a57a477ddf8724730ec7256462931
mwhite98 <43861408+mwhite98@users.noreply.github.com>
2020-04-06 12:49:51 -0700

    Add Vehicle (#2)
    * mvp
    
    * solving mazes
    
    * calling it a night
    
    * fix out of bounds warning

--- drawing.js
+++ drawing.js
@@ -1,79 +1,52 @@
 /*
     Base code from "The Nature of Code", Daniel Shiffman, http://natureofcode.com
     Code based on text "Artificial Intelligence", George Luger
-
 */
 
-// A list of points we will use to "train" the perceptron
-let training = new Array(2000);
-// A Perceptron object
-let ptron;
-// Input mic
-let mic;
+let rocket;
+let desired;
+let obstacles;
+let numObstacles = 8;
+let hitObstacle = false;
+let border_width = 750;
+let border_height = 350;
 
-// We will train the perceptron with one "Point" object at a time
-let count = 0;
-
-// Coordinate space
-let xmin = -1;
-let ymin = -1;
-let xmax = 1;
-let ymax = 1;
-
-// The function to describe a line
-function f(x) {
-  let y = 0.3 * x + 0.4;
-  return y;
+function makeObstacles() {
+  obstacles = new Array(numObstacles);
+  let o_width = 5;
+  let o_height = 50;
+  for (let i=0; i < numObstacles; i++) {
+    obstacles[i] = new Obstacle(random(50, border_width), random(50, border_height), o_width, o_height);
+  }
 }
 
 function setup() {
-  let cnv = createCanvas(600, 600);
-  cnv.mousePressed(userStartAudio);
-  textAlign(CENTER);
-  mic = new p5.AudioIn();
-  mic.start();
-
-  // The perceptron has 3 inputs -- x, y, and bias
-  // Second value is "Learning Constant"
-  ptron = new Perceptron(3, 0.001); // Learning Constant is low just b/c it's fun to watch, this is not necessarily optimal
-
-  // Create a random set of training points and calculate the "known" answer
-  for (let i = 0; i < training.length; i++) {
-    let x = random(xmin, xmax);
-    let y = random(ymin, ymax);
-    let answer = 1;
-    if (y < f(x)) answer = -1;
-    training[i] = {
-      input: [x, y, 1],
-      output: answer
-    };
-  }
+  createCanvas(800, 400);
+  desired = new p5.Vector(border_width - border_width/8, border_height/2);
+  makeObstacles();
+  rocket = new Vehicle(obstacles.length, 0, random(border_height));
 }
 
-
 function draw() {
-  background(0);
-  fill(255);
-  text('tap to start', width/2, 20);
+  background('pink');
+  fill('blue');
+  ellipse(desired.x, desired.y, 36, 36);
 
-  volume = mic.getLevel();
-
-  // Train the Perceptron with one "training" point at a time
-  ptron.train(training[count].input, training[count].output);
-  count = (count + 1) % training.length;
-
-  // Draw all the points based on what the Perceptron would "guess"
-  // Does not use the "known" correct answer
-  for (let i = 0; i < count; i++) {
-    stroke(255);
-    strokeWeight(1);
-    fill('red');
-    let guess = ptron.feedforward(training[i].input);
-    if (guess > 0) noFill();
-
-    let x = map(training[i].input[0], xmin, xmax, 0, width);
-    let y = map(training[i].input[1], ymin, ymax, height, 0);
-
-    ellipse(x, y, volume * 40, volume * 40);
+  // Draw the obstacles
+  for (let i = 0; i < obstacles.length; i++) {
+    obstacles[i].display();
+    if (obstacles[i].contains(rocket.position)) {
+      hitObstacle = true;
+    }
   }
+
+  if (rocket.position.x > width || rocket.position.y > height || rocket.position.x < 0 || rocket.position.y < 0) {
+    fill(50);
+    text("rocket is out of bounds (but it'll be back!)", width/2, 30);
+  }
+
+  rocket.steer(obstacles, desired);
+  rocket.update(hitObstacle);
+  hitObstacle = false;
+  rocket.display();
 }
---
+++ vehicle.js
@@ -0,0 +1,104 @@
+/*
+    Base code from "The Nature of Code", Daniel Shiffman, http://natureofcode.com
+*/
+
+class Vehicle {
+    constructor(n, x, y) {
+        this.brain = new Perceptron(n, 0.001);
+        this.acceleration = createVector(0, 0);
+        this.velocity = createVector(0, -2);
+        this.position = createVector(x, y);
+        this.r = 6;
+        this.maxspeed = 3;
+        this.maxforce = 0.1;
+    }
+
+    // Method to update location
+    update(didHit) {
+        if (didHit) {
+            this.velocity = createVector(0, -2);
+            this.position = createVector(0, random(700));
+        } else {
+            // Update velocity
+            this.velocity.add(this.acceleration);
+            // Limit speed
+            this.velocity.limit(this.maxspeed);
+            this.position.add(this.velocity);
+        }
+        // Reset acceleration to 0 each cycle
+        this.acceleration.mult(0);
+    }
+
+    applyForce(force) {
+        this.acceleration.add(force);
+    }
+      
+    repel(obstacle) {
+        var direction = p5.Vector.sub(obstacle.position, this.position); // A vector pointing from the location to the target
+        let distance = direction.mag();
+        direction.setMag(this.maxspeed);
+        let force = -150.0 / (distance * distance);
+        direction.mult(force);
+        return direction;
+    }
+
+    steer(obstacles, goal) {
+        let forces = new Array(obstacles.length);
+        let closestObstacle;
+        let tempDifference;
+        let difference = Infinity;
+
+        // Determining repelling forces for all obstacles
+        // Also determines current closest obstacle to the rocket
+        for (let i = 0; i < forces.length; i++) {
+            forces[i] = this.repel(obstacles[i]);
+            tempDifference = p5.Vector.sub(obstacles[i].position, this.position);
+            if (tempDifference.mag() < difference) {
+                difference = tempDifference.mag();
+                closestObstacle = obstacles[i].position;
+            }
+        }
+
+        // Sets vector for moving towards the end goal
+        let desired = p5.Vector.sub(goal, this.position);
+        let d = desired.mag();
+        if (d < 100) {
+            var m = map(d, 0, 100, 0, this.maxspeed);
+            desired.setMag(m);
+        } else {
+            desired.setMag(this.maxspeed);
+        }
+
+        let steer = p5.Vector.sub(desired, this.velocity);
+        steer.limit(this.maxforce);  // Limit to maximum steering force
+        this.applyForce(steer);
+
+        // All the steering forces are inputs
+        var result = this.brain.feedforward(forces);
+        this.applyForce(result);
+
+        // If you're closer to an obstacle than the end goal, train perceptron!
+        if (desired.mag() > difference) {
+            let error = p5.Vector.sub(closestObstacle, this.position);
+            error = 1 / (error.limit(this.maxforce));
+            this.brain.train(forces, error);
+        }
+    }
+
+    display() {
+        // Draw a triangle rotated in the direction of velocity
+        var theta = this.velocity.heading() + PI / 2;
+        fill(127);
+        stroke(200);
+        strokeWeight(1);
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(theta);
+        beginShape();
+        vertex(0, -this.r * 2);
+        vertex(-this.r, this.r * 2);
+        vertex(this.r, this.r * 2);
+        endShape(CLOSE);
+        pop();
+    }
+}
